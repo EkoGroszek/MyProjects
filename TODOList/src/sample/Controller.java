872b9b2fa8b438a21aa78f79dc6317ee59affbf3
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-02-03 15:44:24 +0100

    Added ToDoItem class and some samples

--- TODOList/src/sample/Controller.java
+++ TODOList/src/sample/Controller.java
@@ -1,4 +1,29 @@
 package sample;
 
+import sample.datamodel.ToDoItem;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+
+    private List<ToDoItem> toDoItems;                   //lista naszych zadań
+
+    public void initialize() {
+        ToDoItem item1 = new ToDoItem("Testowe przypomnienie o urodzinach", "Kup Oli coś na urodziny dzbanie", LocalDate.of(2019, Month.APRIL, 25));
+        ToDoItem item2 = new ToDoItem("Lekarz", "Idź do lekarza dzbanie ", LocalDate.of(2019, Month.MAY, 12));
+        ToDoItem item3 = new ToDoItem("Skończ projekt", "Skończ ten projekt w końcu ", LocalDate.of(2019, Month.APRIL, 20));
+        ToDoItem item4 = new ToDoItem("Samobój", "Weź sie zabij albo co", LocalDate.of(2019, Month.JANUARY, 28));
+        ToDoItem item5 = new ToDoItem("Zakupy", "Idź na zakupy kup mleko", LocalDate.of(2019, Month.MARCH, 1));
+
+        toDoItems = new ArrayList<>();
+        toDoItems.add(item1);
+        toDoItems.add(item2);
+        toDoItems.add(item3);
+        toDoItems.add(item4);
+        toDoItems.add(item5);
+
+    }
 }
---
+++ TODOList/src/sample/datamodel/ToDoItem.java
@@ -0,0 +1,39 @@
+package sample.datamodel;
+
+import java.time.LocalDate;
+
+public class ToDoItem {
+    private String shortDescription;            //krótki opis
+    private String details;                     // detale zadania
+    private LocalDate deadline;                 // termin wykonania zadania
+
+    public ToDoItem(String shortDescription, String details, LocalDate deadline) {
+        this.shortDescription = shortDescription;
+        this.details = details;
+        this.deadline = deadline;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+}
